ec81415e9092758d544f80b39bf71561567e6ac9
rezoKom <rezokomakhidze1@gmail.com>
2020-09-30 18:34:37 +0400

    added create order controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    #users
     post "login", to: 'users#login'
     post "signup", to: 'users#signup'
     post "logout", to: 'users#logout'
+    #orders
+    post "orders", to: 'orders#create'
   end
   root "application#home"
 end
