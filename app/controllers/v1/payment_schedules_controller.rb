cd6c8f50e2ac6b84a27ad290c9ed1b4cce1e4dcb
rezoKom <rezokomakhidze1@gmail.com>
2020-10-18 22:51:29 +0400

    updated payment_schedule controller

--- app/controllers/v1/payment_schedules_controller.rb
+++ app/controllers/v1/payment_schedules_controller.rb
@@ -1,7 +1,8 @@
 class V1::PaymentSchedulesController < ApplicationController
     def create
-        schedule = IceCube::Schedule.new(now = Time.now)
-        schedule.add_recurrence_rule IceCube::Rule.monthly.day_of_month(params[:day])
+        date = Date.parse(params[:start_date])
+        schedule = IceCube::Schedule.new(date)
+        schedule.add_recurrence_rule IceCube::Rule.monthly.day_of_month(date.day)
         hash_schedule = schedule.to_hash
         payment_schedule = PaymentSchedule.new schedule: hash_schedule, client_id: params[:client_id]
         if payment_schedule.save
@@ -13,9 +14,11 @@ class V1::PaymentSchedulesController < ApplicationController
 
     def get
         client = Client.find params[:client_id]
+        drop_num = 12 - client.payment_schedule.length
         payment_schedule = client.payment_schedules.first
         schedule = IceCube::Schedule.from_hash(payment_schedule.schedule)
-        show = schedule.first 12
+        raw_schedule = schedule.first 12 
+        show = raw_schedule.drop drop_num
         render json: {success: true, schedule: show}
     end
 end
