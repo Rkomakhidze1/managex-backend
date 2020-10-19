cfea50e4520c14ca87962a0568d5be2389dd075d
rezoKom <rezokomakhidze1@gmail.com>
2020-10-19 22:12:16 +0400

    fixed small bugs for clients controller

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -1,18 +1,18 @@
 class V1::ClientsController < ApplicationController
 
     def get
-        client = Client.find params[:client_id]
+        client = Client.find schedule_params[:client_id]
         render json:{success: true, client: client}, status: :ok
     end
 
     def pay
-        client = Client.find params[:client_id]
-        updated_schedule = update_payment_schedule client.payment_schedule, params[:payment]
+        client = Client.find schedule_params[:client_id]
+        updated_schedule = update_payment_schedule client.payment_schedule, schedule_params[:payment]
         client.already_paid = client.full_payment - sum(updated_schedule)
         client.has_to_pay = client.full_payment - client.already_paid
         client.payment_schedule = updated_schedule
         if client.save
-        render json: {success: true, schedule: updated_schedule}
+            render json: {success: true, schedule: updated_schedule}
         else
             render json: {success: false, data: client.errors.full_messages}
         end
@@ -20,7 +20,13 @@ class V1::ClientsController < ApplicationController
 
     private
 
+    def schedule_params
+        params.permit(:client_id, :payment)
+    end
+
     def update_payment_schedule(current_schedule, payment)
+        return [] if current_schedule == []
+
         val = current_schedule.first - payment
         if (val == 0) 
             updated_schedule = current_schedule.drop 1
