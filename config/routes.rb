49488b9c7f4c1b052836e1beb6a889f947fded84
rezoKom <rezokomakhidze1@gmail.com>
2020-11-05 15:13:06 +0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     post "signup", to: 'users#signup'
     post "logout", to: 'users#logout'
     post "me", to: 'users#me'
+    post "test", to: 'users#test'
     #orders
     post "createOrder", to: 'orders#create' 
     post "getOrders", to: 'orders#get'
